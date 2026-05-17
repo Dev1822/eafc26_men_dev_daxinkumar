386463761704a59c1ace24b3c395ec8ebd825125
Dev1822 <dev.d.patel.cg@gmail.com>
2026-05-17 18:29:57 +0530

    MongoDB Connected

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();  
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB Connected Successfully");
+  } catch (err) {
+    console.log("Database connection error", err);
+  }
+};
+
+module.exports = connectDB;
