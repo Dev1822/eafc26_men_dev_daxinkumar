c182aa5567b445d50da35c95932d55b4c5de98b8
Dev1822 <dev.d.patel.cg@gmail.com>
2026-05-23 19:21:00 +0530

    Add PUT /players/:id endpoint for complete player replacement

--- backend/src/routes/playerRoutes.js
+++ backend/src/routes/playerRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getPlayers, getPlayerById, createPlayer } = require('../controllers/playerController');
+const { getPlayers, getPlayerById, createPlayer, replacePlayer } = require('../controllers/playerController');
 
 router.route('/')
     .get(getPlayers)
     .post(createPlayer);
-router.route('/:id').get(getPlayerById);
+router.route('/:id')
+    .get(getPlayerById)
+    .put(replacePlayer);
 
 module.exports = router;
